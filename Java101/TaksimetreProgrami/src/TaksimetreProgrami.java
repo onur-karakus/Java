7efd37dd8709c374e7711f15a442c0d209bd35a1
Onur Karakuş <33610612+onur-karakus@users.noreply.github.com>
2023-05-07 23:28:10 +0300

    Update TaksimetreProgrami.java

--- Java101/TaksimetreProgrami/src/TaksimetreProgrami.java
+++ Java101/TaksimetreProgrami/src/TaksimetreProgrami.java
@@ -3,17 +3,26 @@ package TaksimetreProgrami;
 import java.util.Scanner;
 public class TaksimetreProgrami {
     public static void main(String[] args) {
+        
+        //Değişkenlerin tanımlanması işlemi
         int km;
         double perKm = 2.20, total, startPrice = 10;
-
+        
+        //Scanner sıfınının tanımlanması işlemi
         Scanner input = new Scanner(System.in);
+        
+        //Kullanıcıdan gidilen mesafe değerinin alınması işlemi
         System.out.print("Mesafeyi KM cinsinden giriniz : ");
         km = input.nextInt();
-
+        
+        //Toplam mesafenin hesaplanması işlemi
         total = (km * perKm);
         total += startPrice;
-
+        
+        //Toplam tutarın 20 TL'den az olmaması koşulunun sağlanması
         total = ( total < 20) ? 20 : total;
+        
+        //Toplam tutarın ekrana yazdırılması işlemi
         System.out.println("Toplam Tutar : " + total + " TL");
 
     }
