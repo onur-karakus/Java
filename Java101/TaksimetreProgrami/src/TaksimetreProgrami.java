80b3fea211447b30f378cc41cedcfd12af6d1df9
Onur KARAKUŞ <onurk27.ok@gmail.com>
2023-05-08 00:26:08 +0300

    Taksimetre Programı

--- Java101/TaksimetreProgrami/src/TaksimetreProgrami.java
+++ Java101/TaksimetreProgrami/src/TaksimetreProgrami.java
@@ -1,5 +1,6 @@
 package TaksimetreProgrami;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class TaksimetreProgrami {
@@ -7,9 +8,9 @@ public class TaksimetreProgrami {
 
         //Değişkenlerin tanımlanması işlemi
         int km;
-        double perKm = 2.20, total, startPrice = 10;
+        double perKm = 2.25, total, startPrice = 10;
 
-        //Scanner sıfınının tanımlanması işlemi
+        //Scanner sınıfının tanımlanması işlemi
         Scanner input = new Scanner(System.in);
 
         //Kullanıcıdan gidilen mesafe değerinin alınması işlemi
@@ -22,9 +23,11 @@ public class TaksimetreProgrami {
 
         //Toplam tutarın 20 TL'den az olmaması koşulunun sağlanması
         total = (total < 20) ? 20 : total;
+        DecimalFormat formatter = new DecimalFormat("##.##");
+
 
         //Toplam tutarın ekrana yazdırılması işlemi
-        System.out.println("Toplam Tutar : " + total + " TL");
+        System.out.println("Toplam Tutar : " + formatter.format(total) + " TL");
 
     }
 }
