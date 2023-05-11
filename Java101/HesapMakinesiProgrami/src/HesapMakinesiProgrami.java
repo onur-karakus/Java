16a4b3e24b89de3b3963ee3bcd73f2faaa1ee867
Onur KARAKUŞ <onurk27.ok@gmail.com>
2023-05-11 23:44:59 +0300

    Hesap Makinesi

--- Java101/HesapMakinesiProgrami/src/HesapMakinesiProgrami.java
+++ Java101/HesapMakinesiProgrami/src/HesapMakinesiProgrami.java
@@ -10,35 +10,33 @@ public class HesapMakinesiProgrami {
         System.out.print("İkinci Sayıyı Giriniz : ");
         int num2 = scanner.nextInt();
 
-        System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpman\n4-Bölme");
+        System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
         char operator = scanner.next().charAt(0);
 
-        int result = 0;
+        int result;
 
         switch (operator) {
-            case '1':
+            case '1' -> {
                 result = num1 + num2;
                 System.out.println(num1 + " + " + num2 + " = " + result);
-                break;
+            }
 
-            case '2':
+            case '2' -> {
                 result = num1 - num2;
                 System.out.println(num1 + " - " + num2 + " = " + result);
-                break;
+            }
 
-            case '3':
+            case '3' -> {
                 result = num1 * num2;
                 System.out.println(num1 + " * " + num2 + " = " + result);
-                break;
+            }
 
-            case '4':
+            case '4' -> {
                 result = num1 / num2;
                 System.out.println(num1 + " / " + num2 + " = " + result);
-                break;
+            }
 
-            default:
-                System.out.println("Hatalı Giriş Yaptınız!");
-                return;
+            default -> System.out.println("Hatalı Giriş Yaptınız!");
         }
     }
 }
