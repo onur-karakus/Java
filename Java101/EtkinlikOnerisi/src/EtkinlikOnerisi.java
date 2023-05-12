a7c9e9e5f3743f128bae3c9911dd5c776bff2693
Onur KARAKUŞ <onurk27.ok@gmail.com>
2023-05-12 22:48:43 +0300

    Etkinlik Önerisi

---
+++ Java101/EtkinlikOnerisi/src/EtkinlikOnerisi.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class EtkinlikOnerisi {
+    public static void main(String[] args) {
+        int heat;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Hava Sıcaklığını Giriniz : ");
+        heat = input.nextInt();
+
+        if (heat < 5) {
+            System.out.println("Kayak yapabilirsiniz.");
+        } else if (heat <= 14) {
+            System.out.println("Sinemaya gidebilirsiniz.");
+        } else if (heat == 15) {
+            System.out.println("Sinemaya gidebilirsiniz.");
+            System.out.println("Pikniğe gidebilirsiniz.");
+        } else if (heat < 25) {
+            System.out.println("Pikniğe gidebilirsiniz.");
+        } else {
+            System.out.println("Yüzmeye gidebilirsiniz.");
+        }
+    }
+}
