103ea7eb9320b8a4c9f58670343a1e6b5689ca1f
Onur KARAKUŞ <onurk27.ok@gmail.com>
2023-05-15 01:40:46 +0300

    Mükemmel Sayı Bulan Program

---
+++ Java101/MukemmelSayiBulma/src/MukemmelSayiBulma.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MukemmelSayiBulma {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir sayı girin: ");
+        int number = input.nextInt();
+
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+
+        if (sum == number) {
+            System.out.println(number + " mükemmel sayıdır.");
+        } else {
+            System.out.println(number + " mükemmel sayı değildir.");
+        }
+    }
+}
